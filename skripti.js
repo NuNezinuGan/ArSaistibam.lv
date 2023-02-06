6263403849f926acc3de1babf56ef1c153184814
NuNezinuGan <pleksteivars@gmail.com>
2023-02-06 15:00:30 +0200

    Create skripti.js

---
+++ skripti.js
@@ -0,0 +1,12 @@
+//alert("Sveiks! ;)")
+console.log("Sveiks! ;)");
+
+var logs = document.querySelector(".chataZinas");
+var vards = document.querySelector("#vards");
+var zinas = document.querySelector("#zina");
+
+function sutitZinu(){
+
+    console.log("poga iet");
+    console.log("ziņa ir:"+zinas.value);
+}
